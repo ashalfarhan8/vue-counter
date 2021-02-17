fa257e131b89b5535458ce07adb95bf3f15f9385
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-18 03:30:50 +0700

    create reusable component

---
+++ src/components/Container.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-11/12 mx-auto" :class="moreClass">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    moreClass: String,
+  },
+};
+</script>
