2accbc6496425d07c532d2f54d08dfbc6a8cbe4c
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-18 03:31:04 +0700

    implement container

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,23 @@
 <template>
   <header class="px-4 py-4">
-    <div class="flex justify-center items-center">
-      <h1 class="text-3xl font-bold">
-        This is a counter app using
-        <span class="uppercase text-green-600">vue</span> for my exercise
-      </h1>
-    </div>
+    <Container>
+      <div class="flex justify-center items-center">
+        <h1 class="text-3xl font-bold">
+          This is a counter app using
+          <span class="uppercase text-green-600">vue</span> for my exercise
+        </h1>
+      </div>
+    </Container>
   </header>
 </template>
 
 <script>
+import Container from "./Container";
+
 export default {
   name: "Header",
+  components: {
+    Container,
+  },
 };
 </script>
