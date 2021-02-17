8844cbf18da081a8602db855104e8b689c8e4af1
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-18 03:30:33 +0700

    change import two different components api used

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Counter from "./components/Counter.vue";
+import Header from "@/components/Header.vue";
+// import Counter from "@/components/CounterSetup.vue";
+import Counter from "@/components/CounterComposition.vue";
 
 export default {
   name: "App",
